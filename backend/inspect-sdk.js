8fde10b53fed80ca9e372bd20c9d820f698239a5
jagratidod <dodjagrati@gmail.com>
2026-04-13 17:23:46 +0530

    pull

---
+++ backend/inspect-sdk.js
@@ -0,0 +1,4 @@
+import { StandardCheckoutClient } from '@phonepe-pg/pg-sdk-node';
+
+console.log("StandardCheckoutClient methods:", Object.getOwnPropertyNames(StandardCheckoutClient));
+console.log("StandardCheckoutClient.getInstance signature:", StandardCheckoutClient.getInstance.toString());
